380190a05f4b1bd76f20334b867740e46e0202f7
happychildren <vojja01@gmail.com>
2023-01-20 15:57:57 +0100

    img fix

--- UI/slider.php
+++ UI/slider.php
@@ -1,5 +1,6 @@
 <?php
 function nv_ui_slider () {
+	$templ_dir = get_template_directory_uri();
 	wp_enqueue_script( "lightbox", $templ_dir . "/assets/slider.js" );
 }
 add_action( "nv_load_modules", "nv_ui_slider" );
